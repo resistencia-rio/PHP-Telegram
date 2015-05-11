5ffbb10369b80eab3e7749cc34f080fc421f3e05
vitormattos <vitor.mattos@xeriph.com.br>
2015-05-11 09:38:58 -0300

    modificação de diretórios

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
 do {
     if(!shell_exec('pgrep telegram-cli')) {
-        $cmd = __DIR__."/../tg/bin/telegram-cli -c ".__DIR__."/../tg-conf/config -k ".__DIR__."/../tg-conf/telegram.pub -P 2015 > ".__DIR__."/../tg-config/log &";
+        $cmd = __DIR__."/../tg/bin/telegram-cli -k ~/.ssh/telegram.pub -P 2015 &";
         pclose(popen($cmd, 'r'));
     }
     // micro second is one millionth of a second
